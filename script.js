4e57efc294371fea89ac5216b0e46b33a262d780
kenneth caetano <00001134487150sp@al.educacao.sp.gov.br>
2026-02-11 20:23:51 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function calcular(){
+
+    let audio = document.getElementById("audioBeta");
+
+    audio.currentTime = 0;
+    audio.play();
+
+    document.getElementById("resultado").innerText =
+    "⚠️ beta detectado\n💀 0000,0000 — ele percebeu que o universo nao se importa com ele";
+
+}

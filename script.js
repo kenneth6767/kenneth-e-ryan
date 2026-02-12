5fb382e4350e78346ab5557c4aa75ec15cf20ee2
kenneth caetano <00001134487150sp@al.educacao.sp.gov.br>
2026-02-12 18:20:04 -0300

    Enhance script.js with CSS styling
    Added CSS styles for body, headings, and container elements.

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
-function calcular(){
-
-    let audio = document.getElementById("audioBeta");
-
-    audio.currentTime = 0;
-    audio.play();
-
-    document.getElementById("resultado").innerText =
-    "⚠️ beta detectado\n💀 =0000,0000 — ele percebeu que o universo nao se importa com ele";
-
+body{
+    background:black;
+    color:white;
+    font-family:Arial;
+    text-align:center;
 }
+
+h1{
+    font-size:50px;
+}
+
+.container{
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    gap:20px;
+}
+
+.foto{
+    width:300px;
+    border-radius:20px;
+    border:4px solid white;
+}
+
+.calc{
+    background:#111;
+    padding:20px;
+    border-radius:15px;
+}
+
+input{
+    padding:10px;
+    font-size:18px;
+}
+
+button{
+    padding:10px 20px;
+    font-size:18px;
+    margin-top:10px;
+    cursor:pointer;
+}
+
+.desenhos{
+    font-size:40px;
+}
+
 

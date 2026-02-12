e97b5701aff8556c755c6105e26ca0fc3d6340f8
kenneth caetano <00001134487150sp@al.educacao.sp.gov.br>
2026-02-12 20:24:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function calcular(){
 
+    alert("clicou"); // teste pra ver se botão funciona
+
     let audio = document.getElementById("audioBeta");
 
-    audio.currentTime = 0;
-    audio.play();
+    if(audio){
+        audio.currentTime = 0;
+        audio.play().catch(e => console.log("erro audio:", e));
+    }else{
+        alert("audio nao encontrado");
+    }
 
     document.getElementById("resultado").innerText =
-    "⚠️ beta detectado\n💀 0000,0000 — ele percebeu que o universo nao se importa com ele";
+    "⚠️ beta detectado\n💀 =0000,0000 — ele percebeu que o universo nao se importa com ele";
 
 }
 
